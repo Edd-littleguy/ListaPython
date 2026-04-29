10a7964471c7cdd7a559d20cd1592fb4992a7b53
Edd-littleguy <edd1thesinner@gmail.com>
2026-04-29 14:04:10 +0000

    ver 0.1 having fun

--- sentinela.py
+++ sentinela.py
@@ -1,1 +1,7 @@
-
+#--variavel\sentinela--
+while true: 
+    comando = input("digite o comando, se precisar parar digite: sair")
+    if comando == "sair": 
+        break
+    print(f"executando: {comando}")
+    
