034ee4dc4548bc472b9a40eef20aa4e605ebfcf9
Edd-littleguy <edd1thesinner@gmail.com>
2026-05-04 07:53:49 -0300

    Update seletor.py
    codigos novos

--- seletor.py
+++ seletor.py
@@ -1,1 +1,8 @@
-
+#seletor de caracteres--
+letra = input("digite uma letra:").upper()
+if letra == 'A':
+  print("voce selecionou a opção A!!!")
+elif letra == 'B':
+  print("voce selecionou a opção B!!!")
+else:
+  print("essa opção não esta disponivel...")
