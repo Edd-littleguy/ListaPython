483ea9fcfcc94451fa8d78bebe58e57d44a01fd2
Edd-littleguy <edd1thesinner@gmail.com>
2026-05-04 11:32:45 +0000

    ver 0.2

--- seletor.py
+++ seletor.py
@@ -1,8 +1,10 @@
-#seletor de caracteres--
-letra = input("digite uma letra:").upper()
+print("‣ Seja bem-vindo(a) seletor de caracteres!!! 🔠 ")
+print("‣ para continuar digite uma letra na caixa e presssione enter quando terminar!")
+
+letra = input("→ digite sua letra aqui: ").upper()
 if letra == 'A':
-  print("voce selecionou a opção A!!!")
+  print("Voce selecionou a opção.... A!!! ✅")
 elif letra == 'B':
-  print("voce selecionou a opção B!!!")
+  print("voce selecionou a opção... B!!! ❎")
 else:
-  print("essa opção não esta disponivel...")
+  print("essa opção não esta disponivel...❎")
