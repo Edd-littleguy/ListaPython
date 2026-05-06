0baee08550487c5dbfd7233d767ce3ec3df46e9a
Edd-littleguy <edd1thesinner@gmail.com>
2026-05-06 11:18:55 -0300

    Update Quizz.py

--- Quizz.py
+++ Quizz.py
@@ -1,6 +1,10 @@
 #--Quizz astronomia--
 time.sleep(3)
 print("Quizz de astronomia!!!")
-
+time.sleep(6)
+print("Regras do quizz:
+      1. abrir outras abas para pesquisa e proibido
+      2. o quiz tera uma maxima de 30 minutos
+      3. boa sorte!")
 
 
