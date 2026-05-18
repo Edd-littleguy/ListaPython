aaaba181f41f6c09a0c7d4bfbb549d502796794d
Edd-littleguy <edd1thesinner@gmail.com>
2026-05-18 11:39:28 +0000

    ver 0.2

---
+++ invest_simulador.py
@@ -0,0 +1,7 @@
+#simulador de investimento--
+deposito_mensal = 50
+total = 0
+for mes in range (1,7):
+    total = total + deposito_mensal
+    print(f"mês {mes}: saldo total = R$ {total}")
+print(f"ao final de 6 meses, você terá {total}")
