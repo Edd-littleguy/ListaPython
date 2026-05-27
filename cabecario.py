e70c4f553a98d391ba92896853738d2a9f414afe
Edd-littleguy <edd1thesinner@gmail.com>
2026-05-27 10:16:20 -0300

    Update cabecario.py

--- cabecario.py
+++ cabecario.py
@@ -1,1 +1,10 @@
+### definindo e chamando a função
+def exibir_cabecalho():
+  print("=" * 30)
+  print("SISTEMA DE GESTÃO ESCOLAR")
+  print("versão 1.0 - desenvolvido no codespaces")
+  print("=" * 30)
 
+### chamando a função
+exibir_cabecalho()
+print("menu principal")
