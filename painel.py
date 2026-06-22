3103f4b77fcd8a82929664ce5340aa40e45a8e57
Edd-littleguy <edd1thesinner@gmail.com>
2026-06-22 07:54:52 -0300

    Update painel.py

--- painel.py
+++ painel.py
@@ -1,3 +1,9 @@
 print("painel de atendimento!")
 fila_espera = ["senha 01", "senha 02", "senha 03", "senha 04"]
-print("
+print(f"o próximo paciente a ser atendido: {fila_espera[0]}")
+print("pacientes a seguir: ")
+print(f"paciente {fila_espera[1]}")
+print(f"paciente {fila_espera[2]}")
+print(f"paciente {fila_espera[3]}")
+
+print("aguardem com paciencia a sua vez!,por favor e obrigado")
